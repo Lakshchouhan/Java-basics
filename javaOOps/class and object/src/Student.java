6ab323633bbb2fb17e8df809be4e55fb952d9680
LakshChouhan <lakshraj1970@gmail.com>
2025-07-12 13:24:05 +0530

    first commit

--- javaOOps/class and object/src/Student.java
+++
@@ -1,9 +0,0 @@
- class Student {
-    int id=1; String name="laksh";
-    public static void main(String[] args) {
-    Student s1 = new Student(); //Creating an object or instance
-        //Printing values of the object
-        System.out.println(s1.id); //accessing member through reference variable
-        System.out.println(s1.name);
-     }
-}
